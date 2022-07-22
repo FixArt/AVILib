9538e96ba39defd64ac4dec8a83ec736e45eb428
FixArt <79996282+FixArt@users.noreply.github.com>
2022-07-22 17:48:58 +0300

    Update AVILib.hpp

--- AVILib.hpp
+++ AVILib.hpp
@@ -1038,10 +1038,10 @@ namespace AVIL
              * 
              * @return type* Returned constant array.
              */
-            operator const type*() const
+            operator const type* const() const
             {
                 if(array == nullptr) throw(EFAULT);
-                return (const type*)array;
+                return (const type* const)array;
             }
 
             /**
@@ -1910,6 +1910,145 @@ namespace AVIL
                 return pointedObject + 1;
             }
     };
+
+    // template<class first, class second>
+    // struct pair
+    // {
+    //     private:
+    //         void* variable = nullptr;
+    //         size_t currentType = 0;
+
+    //         void resize(const size_t& newSize)
+    //         {
+    //             if(variable != nullptr)
+    //             {
+    //                 if(currentType == typeid(first).hash_code())
+    //                 {
+    //                     ((first*)variable)->~first();
+    //                 }
+    //                 else
+    //                 {
+    //                     ((second*)variable)->~second();
+    //                 }
+    //                 free(variable);
+    //                 variable = nullptr;
+    //             }
+    //             variable = malloc(newSize);
+    //             if(variable == nullptr) throw(ENOMEM);
+    //         }
+
+    //         template<class type>
+    //         void turnTo()
+    //         {
+    //             if(typeid(type) != typeid(first) and typeid(type) != typeid(second))
+    //             {
+    //                 throw(EINVAL);
+    //             }
+    //             resize(sizeof(type));
+    //             new (&variable) type;
+    //             currentType = typeid(type).hash_code();
+    //         }
+    //     public:
+    //         pair<first, second>() : variable{nullptr}, currentType{0} {}
+
+    //         const size_t& currentHash() const
+    //         {
+    //             return currentType;
+    //         }
+
+    //         template<class type>
+    //         pair<first, second>(const type& assigned)
+    //         {
+    //             turnTo<type>();
+    //             *((type*)variable) = (type&)assigned;
+    //         }
+
+    //         template<class alternativeFirst, class alternativeSecond>
+    //         pair<first, second>(const pair<alternativeFirst, alternativeSecond>& copied)
+    //         {
+    //             if(typeid(first).hash_code() != copied.currentHash() and typeid(second).hash_code() != copied.currentHash())
+    //             {
+    //                 throw(EINVAL);
+    //             }
+    //             if(typeid(first).hash_code() == copied.currentHash())
+    //             {
+    //                 turnTo<first>();
+    //                 *((first*)variable) = (first&)copied;
+    //             }
+    //             else
+    //             {
+    //                 turnTo<second>();
+    //                 *((second*)variable) = (second&)copied;
+    //             }
+    //             currentType = copied.currentHash();
+    //         }
+
+    //         template<class type>
+    //         pair<first, second> operator=(const type& assigned)
+    //         {
+    //             turnTo<type>();
+    //             *((type*)variable) = (type&)assigned;
+    //             return *this;
+    //         }
+
+    //         template<class alternativeFirst, class alternativeSecond>
+    //         pair<first, second> operator=(const pair<alternativeFirst, alternativeSecond>& copied)
+    //         {
+    //             if(typeid(first).hash_code() != copied.currentHash() and typeid(second).hash_code() != copied.currentHash())
+    //             {
+    //                 throw(EINVAL);
+    //             }
+    //             if(typeid(first).hash_code() == copied.currentHash())
+    //             {
+    //                 turnTo<first>();
+    //                 *((first*)variable) = (first&)copied;
+    //             }
+    //             else
+    //             {
+    //                 turnTo<second>();
+    //                 *((second*)variable) = (second&)copied;
+    //             }
+    //             currentType = copied.currentHash();
+    //             return *this;
+    //         }
+
+    //         template<class type>
+    //         operator type&()
+    //         {
+    //             if(currentHash() != typeid(type).hash_code())
+    //             {
+    //                 throw(EINVAL);
+    //             }
+    //             return *((type*)variable);
+    //         }
+
+    //         template<class type>
+    //         operator const type&() const
+    //         {
+    //             if(currentType != typeid(type).hash_code())
+    //             {
+    //                 throw(EINVAL);
+    //             }
+    //             return *((type*)variable);
+    //         }
+
+    //         ~pair<first, second>()
+    //         {
+    //             if(variable != nullptr)
+    //             {
+    //                 if(currentType == typeid(first).hash_code())
+    //                 {
+    //                     ((first*)variable)->~first();
+    //                     free((first*)variable);
+    //                 }
+    //                 else
+    //                 {
+    //                     ((second*)variable)->~second();
+    //                     free((second*)variable);
+    //                 }
+    //             }
+    //         }
+    // };
 
     /**
      * @brief Variable without specified type only for Plain Old Data.
